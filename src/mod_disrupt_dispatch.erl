30a66be36caf7996811417feee9b1b4ed689b9ee
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2023-01-04 20:07:11 +0100

    Base module

--- src/mod_disrupt_dispatch.erl
+++ src/mod_disrupt_dispatch.erl
@@ -1,10 +1,26 @@
-{application, zotonic_mod_disrupt_dispatch, [
-    {description, "Module to disrupt base zotonic dispatches."},
-    {vsn, "git"},
-    {registerered, []},
-    {applications, [kernel, stdlib, zotonic_core]},
-    {env, []},
-    {modules, []},
-    {maintainers, []}
-]}.
+%% @author Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
+%% @copyright 2023 Maas-Maarten Zeeman
+%% @doc Disrupt Zotonic base dispatches. 
 
+%% Copyright 2023 Maas-Maarten Zeeman
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(mod_disrupt_dispatch).
+-author("Maas-Maarten Zeeman <mmzeeman@xs4all.nl>").
+
+-mod_title("Disrupt Dispatch").
+-mod_description("Distrupt zotonic base dispatches.").
+-mod_provides([]).
+-mod_depends([disrupt]).
+-mod_prio(5).
