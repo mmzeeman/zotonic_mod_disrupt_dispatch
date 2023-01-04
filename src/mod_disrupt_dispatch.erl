e3b1be14df175789b25eb2034d1fd94b99f20bd7
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2023-01-04 20:03:39 +0100

    Added some base dispatch rules

---
+++ src/mod_disrupt_dispatch.erl
@@ -0,0 +1,10 @@
+{application, zotonic_mod_disrupt_dispatch, [
+    {description, "Module to disrupt base zotonic dispatches."},
+    {vsn, "git"},
+    {registerered, []},
+    {applications, [kernel, stdlib, zotonic_core]},
+    {env, []},
+    {modules, []},
+    {maintainers, []}
+]}.
+
